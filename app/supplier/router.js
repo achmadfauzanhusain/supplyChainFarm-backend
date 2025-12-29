1c495348e3b75130d30194cc2ffa4f869643395e
Fauzan Husain <achmadfauzan1340@gmail.com>
2025-12-30 00:56:38 +0800

    feat(api): supplier:registeredSupplier

--- app/supplier/router.js
+++ app/supplier/router.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { register, notRegisteredSupplier } = require('./controller')
+const { register, notRegisteredSupplier, registeredSupplier } = require('./controller')
 const router = express.Router()
 
 router.post('/register', register)
 router.get('/not-registered', notRegisteredSupplier)
+router.get('/registered', registeredSupplier)
 
 module.exports = router
